6b141053bdfef43da0d2f28e240aa17298f2b058
Mistralys <s.mordziol@gmail.com>
2019-11-09 17:47:08 +0100

    Modified to use the database to cache the data.

--- edit/assets/classes/Items.php
+++ edit/assets/classes/Items.php
@@ -4,9 +4,17 @@ declare(strict_types=1);
 
 namespace FELH;
 
+use AppDB\DBHelper;
+
 class Items
 {
+    const DATA_VERSION = 1;
+    
     const ERROR_UNKNOWN_ITEM_ID = 40201;
+    
+    const ERROR_ROOT_CONTAINER_WRONG_CLASS = 40202;
+    
+    const ERROR_UNKNOWN_TAG_ID = 40203;
     
    /**
     * @var Items_Folder[]
@@ -14,7 +22,7 @@ class Items
     protected $folders = array();
     
    /**
-    * @var Items_Item[]
+    * @var DataType[]
     */
     protected $items = array();
     
@@ -28,23 +36,70 @@ class Items
     */
     protected $request;
     
+   /**
+    * @var string
+    */
+    protected $cacheFile;
+    
+   /**
+    * @var Items_XMLTag[]
+    */
+    protected $tags = array();
+    
+   /**
+    * @var Items_Collection
+    */
+    protected $collection;
+    
     public function __construct(Editor $editor)
     {
         $this->editor = $editor;
         $this->request = $editor->getSite()->getRequest();
+        $this->cacheFile = $editor->getSite()->getWebrootFolder().'/storage/cache.json';
+        $this->collection = new Items_Collection();
+        
+        $this->registerTags();
     }
     
-    public function addFolder(string $label, string $folder) : Items
+    public function addFolder(string $label, string $folder) : Items_Folder
     {
-        $this->folders[] = new Items_Folder($label, $folder);
-        return $this;
+        $folder = new Items_Folder($label, $folder);
+        $this->folders[] = $folder;
+        
+        return $folder;
     }
  
+    protected function registerTag(string $name, int $dbID, string $label)
+    {
+        $this->tags[] = new Items_XMLTag($name, $dbID, $label); 
+    }
+
+    protected function registerTags()
+    {
+        $this->registerTag('GameItemType', 1, t('Items'));
+    }
+    
+   /**
+    * Retrieves the collection instance which is used to
+    * access the XML elements that have been cached in the
+    * database.
+    * 
+    * @return Items_Collection
+    */
+    public function getCollection() : Items_Collection
+    {
+         return $this->collection;
+    }
+    
     public function load() : void
     {
+        DBHelper::startTransaction();
+        
+        DBHelper::deleteRecords('records');
+        
         foreach($this->folders as $folder)
         {
-            $d = new \DirectoryIterator($folder->getPath());
+            $d = new \DirectoryIterator($folder->getXMLPath());
             foreach($d as $item) 
             {
                 if(!$item->isFile()) {
@@ -56,56 +111,80 @@ class Items
                     continue;
                 }
                 
-                $f = fopen($item->getPathname(), 'r');
-                $line = fgets($f); // xml declaration
-                $seekMax = 10;
-                
-                for($i=0; $i < $seekMax; $i++) 
+                $found = array();
+                $content = file_get_contents($item->getPathname());
+                foreach($this->tags as $tag)
                 {
-                    $line = trim(fgets($f));
-                    if(empty($line)) {
-                        continue;
-                    }
+                    $tagName = $tag->getName();
                     
-                    if($line[0] == '<' && substr($line, 0, 4) != '<!--') {
+                    if(strstr($content, '<'.$tagName)) {
+                        $found[] = $tag;
                         break;
                     }
                 }
                 
-                fclose($f);
+                unset($content);
                 
-                if(!stristr($line, 'GameItemTypes')) {
+                if(empty($found)) {
                     continue;
                 }
                 
-                $reader = new Reader($this, $folder, $item->getPathname());
+                $reader = new Reader($this, $folder, $item->getPathname(), $found);
                 $reader->parse();
             }
         }
         
-        usort(
-            $this->items, 
-            function(Items_Item $a, Items_Item $b) {
-                return strnatcasecmp($a->getLabel(), $b->getLabel());
+        foreach($this->items as $item)
+        {
+            if($item instanceof DataType_RootContainer) {
+                $item->toDatabase();
             }
+        }
+        
+        DBHelper::updateDynamic(
+            'config', 
+            array(
+                'date_loaded' => date('Y-m-d H:i:s'),
+                'data_version' => self::DATA_VERSION
+            ), 
+            array()
         );
+        
+        DBHelper::commitTransaction();
     }
     
-    public function addItem(\DOMElement $itemNode, Reader $reader)
+    public function addFromNode(Items_XMLTag $tag, \DOMElement $itemNode, string $xmlFile, Items_Folder $folder)
     {
-        $this->items[] = new Items_Item($this, $itemNode, $reader);
+        $class = '\FELH\Types_'.$tag->getName();
+        
+        $obj = call_user_func(array($class, 'fromNode'), $this, $tag->getName(), $itemNode);
+        
+        if($obj instanceof DataType_RootContainer) 
+        {
+            $obj->setSourceFile($xmlFile);
+            $obj->setFolder($folder);
+            $obj->setTag($tag);
+            $this->items[] = $obj;
+            return;
+        }
+        
+        throw new Exception(
+            'Root container item does not extend DataType_RootContainer',
+            'Class: '.get_class($obj),
+            self::ERROR_ROOT_CONTAINER_WRONG_CLASS
+        );
     }
     
    /**
     * Retrieves all items, sorted by label.
-    * @return Items_Item[]
+    * @return DataType[]
     */
     public function getItems()
     {
         return $this->items;
     }
-    
-    public function getByID(string $id) : Items_Item
+  
+    public function getByID(string $id) : DataType
     {
         foreach($this->items as $item) {
             if($item->getID() === $id) {
@@ -134,7 +213,7 @@ class Items
         return false;
     }
     
-    public function getByRequest() : ?Items_Item
+    public function getByRequest() : ?DataType
     {
         $id = $this->request->getParam('item_id');
         if(!empty($id) && $this->idExists($id)) {
@@ -149,5 +228,47 @@ class Items
         $params['slug'] = 'Items';
         
         return $this->editor->getSite()->buildURL($params);
+    }
+    
+   /**
+    * Retrieves a supported XML tag by its ID.
+    * 
+    * @param int $id
+    * @throws Exception
+    * @return Items_XMLTag
+    */
+    public function getTagByID(int $id) : Items_XMLTag
+    {
+        foreach($this->tags as $tag)
+        {
+            if($tag->getDBID() === $id) {
+                return $tag;
+            }
+        }
+        
+        throw new Exception(
+            'Unknown XML tag ID.',
+            sprintf('Cannot find any tag for the ID [%s].', $id),
+            self::ERROR_UNKNOWN_TAG_ID
+        );
+    }
+    
+   /**
+    * Checks whether the database cache is present, and valid.
+    * @return bool
+    */
+    public function isDatabaseValid() : bool
+    {
+        $config = DBHelper::fetchData('config');
+        
+        if(empty($config)) {
+            return false;
+        }
+        
+        if((int)$config['data_version'] !== self::DATA_VERSION) {
+            return false;
+        }
+        
+        return true;
     }
 }
